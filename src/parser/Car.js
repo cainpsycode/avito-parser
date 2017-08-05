ad353a45ac7ab113d1dc1be7ad43c4eb292710fd
Onorin Aleksey <cainpsycode@gmail.com>
2017-08-06 01:00:05 +0500

    remove js files

--- src/parser/Car.js
+++
@@ -1,1 +0,0 @@
-/// <reference path="./Price.ts" />
--- src/parser/Transmissions.js
+++
@@ -1,8 +0,0 @@
-var Avito;
-(function (Avito) {
-    var Transmissions;
-    (function (Transmissions) {
-        Transmissions[Transmissions["MT"] = 861] = "MT";
-        Transmissions[Transmissions["AT"] = 860] = "AT";
-    })(Transmissions = Avito.Transmissions || (Avito.Transmissions = {}));
-})(Avito || (Avito = {}));
